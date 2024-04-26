561bd5e882f9181d51b181978120771bff95c3f0
Hinata0607 <atahin0607@gmail.com>
2024-04-26 18:32:30 +0900

    手間切り替え実装

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,12 +1,13 @@
 "use client";
-import { Box, Link, useBreakpoint } from "@yamada-ui/react";
+import { Box, Button, Link, Text, useBreakpoint, useColorMode } from "@yamada-ui/react";
 import { Icon } from "@yamada-ui/fontawesome";
-import { faBars, faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faCircleHalfStroke, faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
 import { HeaderButton } from "../elements";
 
 export const Header = () => {
 
     const breakpoint = useBreakpoint();
+    const {toggleColorMode } = useColorMode();
 
     return (
         <>
@@ -21,7 +22,22 @@ export const Header = () => {
         >
             <div style={{color: "#fff", fontWeight: "bold"}}>ここにロゴ</div>
             {["sm", "md"].includes(breakpoint) ?
-            <Icon icon={faBars} />
+            <Box
+                display="flex"
+                flexDirection="column"
+                justifyContent="center"
+                alignItems="center" gap="5px">
+                <Icon icon={faBars}
+                color={["white", "white"]}
+            />
+                <Text
+                    fontSize="xs"
+                    isTruncated
+                    color={["white", "white"]}
+                    >
+                        MENU
+                    </Text>
+            </Box>
             :
             <Box
                 display="flex"
@@ -43,6 +59,11 @@ export const Header = () => {
                 >
                     xxx@xxx.com
                 </HeaderButton>
+
+                <Button
+                    color={["black", "gray"]}
+                    onClick={toggleColorMode}
+                    leftIcon={<Icon icon={faCircleHalfStroke} color={["black", "white"]}/>}/>
             </Box>
             }
         </Box>
