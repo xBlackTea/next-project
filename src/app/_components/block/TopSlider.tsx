e6344a6cca611046f45d0eaf38ef3e0fab7371a6
Hinata0607 <atahin0607@gmail.com>
2024-05-17 12:29:34 +0900

    カルーセルデザイン更新

--- src/app/_components/block/TopSlider.tsx
+++ src/app/_components/block/TopSlider.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Center, Image, useBreakpoint } from "@yamada-ui/react";
+import { Box, Center, Image, useBreakpoint } from "@yamada-ui/react";
 import React from 'react';
 import { Carousel, CarouselSlide } from "@yamada-ui/carousel";
 import { movieCard } from "@/mock/movie/mock";
@@ -23,15 +23,38 @@ export const TopSlider = () => {
                 <CarouselSlide
                     as={Center}
                     key={index}
+                    h="90%"
+                    bgColor="transparent"
+                    overflow="hidden"
+                    sx={{
+                        cursor: "pointer",
+                    }}
                 >
                     <Image
                         src={`/${card.movie_image}`}
                         h="full"
                         aspectRatio="5/3"
                         objectFit="cover"
+                        _hover={{
+                            transform: "scale(1.1)",
+                            transition: "transform 0.3s ease-in-out"
+                        }}
                     />
                 </CarouselSlide>
             ))}
+            <Box
+                position="absolute"
+                top={0}
+                left={0}
+                w="full"
+                h="full"
+                pointerEvents="none"
+                boxShadow={
+                    ["sm", "md"].includes(breakpoint) ?
+                    "inset -50px 0 50px rgba(0, 0, 0, 0.7), inset 50px 0 50px rgba(0, 0, 0, 0.7)" :
+                    "inset -150px 0 50px rgba(0, 0, 0, 0.7), inset 150px 0 50px rgba(0, 0, 0, 0.7)"
+                }
+            />
         </Carousel>
         </>
     );
