3a918405f6b069273fb8b5a32f46b4dc70bec6f4
Hinata0607 <atahin0607@gmail.com>
2024-05-17 10:41:23 +0900

    カルーセル更新

--- src/app/_components/block/TopSlider.tsx
+++ src/app/_components/block/TopSlider.tsx
@@ -1,7 +1,8 @@
 "use client";
-import { Center, useBreakpoint } from "@yamada-ui/react";
+import { Center, Image, useBreakpoint } from "@yamada-ui/react";
 import React from 'react';
 import { Carousel, CarouselSlide } from "@yamada-ui/carousel";
+import { movieCard } from "@/mock/movie/mock";
 
 export const TopSlider = () => {
 
@@ -10,21 +11,27 @@ export const TopSlider = () => {
     return (
         <>
         <Carousel
+            slideSize={["sm"].includes(breakpoint) ? "100%" : ["md"].includes(breakpoint) ? "50%" : ["lg"].includes(breakpoint) ? "50%" : ["xl"].includes(breakpoint) ? "33.3333%" : "25%"}
             size={["sm", "md"].includes(breakpoint) ? "sm" : "md"}
+            as="center"
+            m="20px auto"
+            maxW="2100px"
             autoplay
+            dragFree
         >
-            <CarouselSlide as={Center}  color="#fff" bg="primary">
-                1
-            </CarouselSlide>
-            <CarouselSlide as={Center} color="#fff" bg="secondary">
-                2
-            </CarouselSlide>
-            <CarouselSlide as={Center} color="#fff" bg="warning">
-                3
-            </CarouselSlide>
-            <CarouselSlide as={Center} color="#fff" bg="danger">
-                4
-            </CarouselSlide>
+            {movieCard.map((card, index) => (
+                <CarouselSlide
+                    as={Center}
+                    key={index}
+                >
+                    <Image
+                        src={`/${card.movie_image}`}
+                        h="full"
+                        aspectRatio="5/3"
+                        objectFit="cover"
+                    />
+                </CarouselSlide>
+            ))}
         </Carousel>
         </>
     );
