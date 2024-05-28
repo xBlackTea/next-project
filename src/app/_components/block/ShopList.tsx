636836dcb94ab8fa94a9a1158e46d6617c9de91f
xBlackTea <xBlackTea.Official@gmail.com>
2024-05-28 13:52:45 +0900

    add project

--- src/app/_components/block/ShopList.tsx
+++ src/app/_components/block/ShopList.tsx
@@ -1,31 +1,13 @@
 import React from 'react';
 import { Box, SimpleGrid } from '@yamada-ui/react';
 import ShopInformation from './ShopInformation';
-
-const shop = [
-  {
-    id: 1,
-    shopImage: "cinema_donuts.jpg",
-  },
-  {
-    id: 2,
-    shopImage: "cinema_donuts.jpg",
-  },
-  {
-    id: 3,
-    shopImage: "cinema_donuts.jpg",
-  },
-  {
-    id: 4,
-    shopImage: "cinema_donuts.jpg",
-  },
-];
+import { shopList } from '@/list/shopList';
 
 const ShopList = () => {
   return (
     <Box m="15px auto 0 auto" w="100%">
       <SimpleGrid columns={2} gap={"15px"}>
-        {shop.map((data) => (
+        {shopList.map((data) => (
           <ShopInformation
             key={data.id}
             shopImage={data.shopImage}
---
+++ src/list/shopList.ts
@@ -0,0 +1,23 @@
+export const shopList = [
+  {
+    id: 1,
+    shopImage: "cinema_donuts.jpg",
+  },
+  {
+    id: 2,
+    shopImage: "cinema_donuts.jpg",
+  },
+  {
+    id: 3,
+    shopImage: "cinema_donuts.jpg",
+  },
+  {
+    id: 4,
+    shopImage: "cinema_donuts.jpg",
+  },
+];
+
+export type shopType = {
+  id: number;
+  shopImage: string;
+};
