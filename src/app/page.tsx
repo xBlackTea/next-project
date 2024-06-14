c972d415672986d6f397b01054675a139f043cbf
Hinata0607 <atahin0607@gmail.com>
2024-06-14 10:04:10 +0900

    test: husky commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,65 @@
-"use client";
-import { Box, useBreakpoint } from "@yamada-ui/react";
+'use client';
+import { Box, useBreakpoint } from '@yamada-ui/react';
 
-import React from "react";
-import { movieCard } from "@/mock/movie/mock";
-import { fetchMovieResponse } from "@/mock/movie/MovieInterface";
+import React from 'react';
+import { movieCard } from '@/mock/movie/mock';
+import { fetchMovieResponse } from '@/mock/movie/MovieInterface';
 import {
-  MovieCard,
-  TopSectionContainer,
-  TopSectionHeader,
-  TopSlider,
-} from "./_components/block";
-import TheaterList from "./_components/block/TheaterList";
-import ShopList from "./_components/block/ShopList";
-import { EventList } from "./_components/block/EventList";
-import { ServiceList } from "./_components/block/ServiceList";
+	MovieCard,
+	TopSectionContainer,
+	TopSectionHeader,
+	TopSlider,
+} from './_components/block';
+import TheaterList from './_components/block/TheaterList';
+import ShopList from './_components/block/ShopList';
+import { EventList } from './_components/block/EventList';
+import { ServiceList } from './_components/block/ServiceList';
 
 export default function Page() {
-  const breakpoint = useBreakpoint();
+	const breakpoint = useBreakpoint();
 
-  return (
-    <>
-      <TopSlider />
+	return (
+		<>
+			<TopSlider />
 
-      <TopSectionContainer>
-        <TopSectionHeader title="作品案内" link="/movies" />
-        <Box
-          flexWrap="wrap"
-          display="flex"
-          justifyContent="space-between"
-          alignItems="center"
-        >
-          {movieCard.map((data: fetchMovieResponse) => (
-            <MovieCard
-              key={data.id}
-              movie_image={data.movie_image}
-              title={data.title}
-              id={"a"}
-            />
-          ))}
-        </Box>
-      </TopSectionContainer>
+			<TopSectionContainer>
+				<TopSectionHeader title="作品案内" link="/movies" />
+				<Box
+					flexWrap="wrap"
+					display="flex"
+					justifyContent="space-between"
+					alignItems="center"
+				>
+					{movieCard.map((data: fetchMovieResponse) => (
+						<MovieCard
+							key={data.id}
+							movie_image={data.movie_image}
+							title={data.title}
+							id={data.id}
+						/>
+					))}
+				</Box>
+			</TopSectionContainer>
 
-      <TopSectionContainer>
-        <TopSectionHeader title="劇場案内" link="/theaters" />
-        <TheaterList />
-      </TopSectionContainer>
+			<TopSectionContainer>
+				<TopSectionHeader title="劇場案内" link="/theaters" />
+				<TheaterList />
+			</TopSectionContainer>
 
-      <TopSectionContainer>
-        <TopSectionHeader title="売店案内" link="/shops" />
-        <ShopList />
-      </TopSectionContainer>
+			<TopSectionContainer>
+				<TopSectionHeader title="売店案内" link="/shops" />
+				<ShopList />
+			</TopSectionContainer>
 
-      <TopSectionContainer>
-        <TopSectionHeader title="イベント案内" link="/event" />
-        <EventList />
-      </TopSectionContainer>
+			<TopSectionContainer>
+				<TopSectionHeader title="イベント案内" link="/event" />
+				<EventList />
+			</TopSectionContainer>
 
-      <TopSectionContainer>
-        <TopSectionHeader title="サービス案内" link="/service" />
-        <ServiceList />
-      </TopSectionContainer>
-    </>
-  );
+			<TopSectionContainer>
+				<TopSectionHeader title="サービス案内" link="/service" />
+				<ServiceList />
+			</TopSectionContainer>
+		</>
+	);
 }
