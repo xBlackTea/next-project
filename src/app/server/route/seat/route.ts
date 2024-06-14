bf333e38647a3da40f03b095b655c2910c50159c
yamatatsu1215 <tatsuyama3140@gmail.com>
2024-06-14 11:56:00 +0900

    add seat crudAPI

--- src/app/server/route/seat/route.ts
+++ src/app/server/route/seat/route.ts
@@ -1,1 +1,62 @@
 // seatモデルのAPIを定義
+import { NextRequest, NextResponse } from "next/server";
+import { NextApiRequest, NextApiResponse } from "next";
+import { Pool } from "pg";
+
+// db接続情報
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+});
+
+interface Seat {
+    seat_id: number;
+    seat_point: string;
+}
+
+// GETメソッドの処理
+export async function GET() {
+    const client = await pool.connect();
+    try{
+        const ret = await client.query('SELECT * FROM "Seat"',[]);
+        return NextResponse.json(ret.rows);
+    } catch (error) {
+        console.error('Error executing query', error);
+        return NextResponse.json(
+            { error: "Error executing query" },
+            { status: 500 }
+        );
+    } finally {
+        client.release();
+    }
+}
+
+// POSTメソッドの処理
+export async function POST(req:NextRequest) {
+    try{
+        const { seat_point }: Seat = await req.json();
+        const client = await pool.connect();
+        try {
+            const query = `
+            INSERT INTO "Seat" (seat_point)
+            VALUES ($1)
+            RETURNING *`;
+            const values = [seat_point];
+            const result = await client.query(query,values);
+            return NextResponse.json(result.rows[0], { status: 201 });
+        } catch (error) {
+            console.error("Error executing query", error);
+            return NextResponse.json(
+                { error: "Error executing query" },
+                { status: 500 }
+            );
+        } finally {
+            client.release();
+        }
+    } catch (error) {
+        console.error('Invalid request error', error);
+        return NextResponse.json(
+            { error: 'Invalid request error' },
+            { status: 400 }
+        );
+    }
+}
