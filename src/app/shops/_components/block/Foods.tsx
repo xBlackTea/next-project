8cd7e26d4365ccaac2dcdac009ff013953b8da8f
10yu30 <yuu2003151030@gmail.com>
2024-06-14 12:15:40 +0900

    add shops update

--- src/app/shops/_components/block/Foods.tsx
+++ src/app/shops/_components/block/Foods.tsx
@@ -3,87 +3,83 @@ import React from 'react'
 
 const Foods = () => {
   return (
-    <Box>
-        <Box w="80%" m="20px auto" bgColor="#111" lineHeight="1.5" color="#fff">
-            <Text ml="1vw" fontSize="2rem">フード</Text>
+    <Box bgColor="#fff" color="#000" py="20px">
+      <Box w="80%" m="0 auto" bgColor="#111" lineHeight="1.5" color="#fff" p="10px">
+        <Text ml="1vw" fontSize="2rem">フード</Text>
+      </Box>
+
+      <Box display="flex" flexWrap="wrap" w="80%" m="20px auto" gap="20px">
+
+        {/* ポップコーン */}
+        <Box display="flex" w="calc(50% - 10px)" bgColor="#f3f3f3" borderRadius="8px" boxShadow="0 4px 8px rgba(0, 0, 0, 0.1)" overflow="hidden">
+          <Box minW="230px" w="230px" h="230px" bgColor="#ccc" flexShrink="0">
+            <Image src="/popcorn.jpg" alt="ポップコーン" w="100%" h="100%" objectFit="cover" />
+          </Box>
+          <Box display="flex" flexDirection="column" p="10px" w="100%">
+            <Box display="flex" justifyContent="space-between" borderBottom="1px dotted #aaa" pb="5px" mb="5px">
+              <Text fontSize="1.6rem">ポップコーン</Text>
+              <Text>S(￥300) M(￥400) L(￥500)</Text>
+            </Box>
+            <Box>
+              <Text fontSize="1.5rem">・塩</Text>
+              <Text fontSize="1.5rem">・キャラメル</Text>
+              <Text fontSize="1.5rem">・バターしょうゆ</Text>
+            </Box>
+          </Box>
         </Box>
 
-        <Box display="flex" w="80%" m="10px auto">
-
-            {/* ポップコーン */}
-            <Box display="flex" w="50%">
-                <Box minW="230px" w="230px" h="230px" m="10px" borderRadius="2px" bgColor="#999">
-                <Image src="/popcorn.jpg" alt="HAL Nagoya" w="100%" h="100%" borderRadius="2px"/>
-                </Box>
-                <Box display="flex" flexDirection="column" w="100%">
-                <Box display="flex" m="10px 10px 0 0" justifyContent="space-between" borderBottom="dotted">              
-                    <Text fontSize="1.6rem" m="0 10px 0 0">ポップコーン</Text>
-                    <Text display="flex" alignItems="flex-end">S(￥300) M(￥400) L(￥500)</Text>
-                </Box>
-                <Box>
-                    <Text m="5px 0 0 0" fontSize="1.5rem">・塩</Text>
-                    <Text m="5px 0 0 0" fontSize="1.5rem">・キャラメル</Text>
-                    <Text m="5px 0 0 0" fontSize="1.5rem">・バターしょうゆ</Text>
-                </Box>
-                </Box>
+        {/* ホットドッグ */}
+        <Box display="flex" w="calc(50% - 10px)" bgColor="#f3f3f3" borderRadius="8px" boxShadow="0 4px 8px rgba(0, 0, 0, 0.1)" overflow="hidden">
+          <Box minW="230px" w="230px" h="230px" bgColor="#ccc" flexShrink="0">
+            <Image src="/hotdog.jpg" alt="ホットドッグ" w="100%" h="100%" objectFit="cover" />
+          </Box>
+          <Box display="flex" flexDirection="column" p="10px" w="100%">
+            <Box display="flex" justifyContent="space-between" borderBottom="1px dotted #aaa" pb="5px" mb="5px">
+              <Text fontSize="1.6rem">ホットドッグ</Text>
+              <Text>(￥500)</Text>
             </Box>
-
-            {/* ホットドッグ */}
-            <Box display="flex" w="50%">
-                <Box minW="230px" w="230px" h="230px" m="10px" borderRadius="2px" bgColor="#999">
-                <Image src="/hotdog.jpg" alt="HAL Nagoya" w="100%" h="100%" borderRadius="2px"/>
-                </Box>
-                <Box display="flex" flexDirection="column" w="100%">
-                <Box display="flex" m="10px 10px 0 0" justifyContent="space-between" borderBottom="dotted">              
-                    <Text fontSize="1.6rem" m="0 10px 0 0">ホットドッグ</Text>
-                    <Text display="flex" alignItems="flex-end">(￥500)</Text>
-                </Box>
-                <Box>
-                    <Text m="5px 0 0 0" fontSize="1.5rem">・ケチャップ&amp;マスタード</Text>
-                    <Text m="5px 0 0 0" fontSize="1.5rem">・チーズ&amp;チリソース</Text>
-                </Box>
-                </Box>
+            <Box>
+              <Text fontSize="1.5rem">・ケチャップ&amp;マスタード</Text>
+              <Text fontSize="1.5rem">・チーズ&amp;チリソース</Text>
             </Box>
-            </Box>
-
-
-            <Box display="flex" w="80%" m="10px auto">
-
-            {/* チュロス */}
-            <Box display="flex" w="50%">
-                <Box minW="230px" w="230px" h="230px" m="10px" borderRadius="2px" bgColor="#999">
-                <Image src="/churros.jpg" alt="HAL Nagoya" w="100%" h="100%" borderRadius="2px"/>
-                </Box>
-                <Box display="flex" flexDirection="column" w="100%">
-                <Box display="flex" m="10px 10px 0 0" justifyContent="space-between" borderBottom="dotted">              
-                    <Text fontSize="1.6rem" m="0 10px 0 0">チュロス</Text>
-                    <Text display="flex" alignItems="flex-end">S(￥300) W(￥500)</Text>
-                </Box>
-                <Box>
-                    <Text m="5px 0 0 0" fontSize="1.5rem">・シナモン</Text>
-                    <Text m="5px 0 0 0" fontSize="1.5rem">・チョコ</Text>
-                </Box>
-                </Box>
-            </Box>
-
-            {/* ポテト */}
-            <Box display="flex" w="50%">
-                <Box minW="230px" w="230px" h="230px" m="10px" borderRadius="2px" bgColor="#999">
-                <Image src="/potato.jpg" alt="HAL Nagoya" w="100%" h="100%" borderRadius="2px"/>
-                </Box>
-                <Box display="flex" flexDirection="column" w="100%">
-                <Box display="flex" m="10px 10px 0 0" justifyContent="space-between" borderBottom="dotted">              
-                    <Text fontSize="1.6rem" m="0 10px 0 0">ポテト</Text>
-                    <Text display="flex" alignItems="flex-end">S(￥300) M(￥400) L(￥500)</Text>
-                </Box>
-                <Box>
-                    <Text m="5px 0 0 0" fontSize="1.5rem">・ケチャップ&amp;マスタード</Text>
-                    <Text m="5px 0 0 0" fontSize="1.5rem">・チーズ&amp;チリソース</Text>
-                </Box>
-                </Box>
-            </Box>
+          </Box>
         </Box>
-      
+
+        {/* チュロス */}
+        <Box display="flex" w="calc(50% - 10px)" bgColor="#f3f3f3" borderRadius="8px" boxShadow="0 4px 8px rgba(0, 0, 0, 0.1)" overflow="hidden">
+          <Box minW="230px" w="230px" h="230px" bgColor="#ccc" flexShrink="0">
+            <Image src="/churros.jpg" alt="チュロス" w="100%" h="100%" objectFit="cover" />
+          </Box>
+          <Box display="flex" flexDirection="column" p="10px" w="100%">
+            <Box display="flex" justifyContent="space-between" borderBottom="1px dotted #aaa" pb="5px" mb="5px">
+              <Text fontSize="1.6rem">チュロス</Text>
+              <Text>S(￥300) W(￥500)</Text>
+            </Box>
+            <Box>
+              <Text fontSize="1.5rem">・シナモン</Text>
+              <Text fontSize="1.5rem">・チョコ</Text>
+            </Box>
+          </Box>
+        </Box>
+
+        {/* ポテト */}
+        <Box display="flex" w="calc(50% - 10px)" bgColor="#f3f3f3" borderRadius="8px" boxShadow="0 4px 8px rgba(0, 0, 0, 0.1)" overflow="hidden">
+          <Box minW="230px" w="230px" h="230px" bgColor="#ccc" flexShrink="0">
+            <Image src="/potato.jpg" alt="ポテト" w="100%" h="100%" objectFit="cover" />
+          </Box>
+          <Box display="flex" flexDirection="column" p="10px" w="100%">
+            <Box display="flex" justifyContent="space-between" borderBottom="1px dotted #aaa" pb="5px" mb="5px">
+              <Text fontSize="1.6rem">ポテト</Text>
+              <Text>S(￥300) M(￥400) L(￥500)</Text>
+            </Box>
+            <Box>
+              <Text fontSize="1.5rem">・ケチャップ&amp;マスタード</Text>
+              <Text fontSize="1.5rem">・チーズ&amp;チリソース</Text>
+            </Box>
+          </Box>
+        </Box>
+
+      </Box>
     </Box>
   )
 }
