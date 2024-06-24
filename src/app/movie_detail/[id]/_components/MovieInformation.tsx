e40afe383eacaba5810718f9a8ac7c3779a0f670
dango-the-world <z.rohaso@gmail.com>
2024-06-24 16:12:08 +0900

    update MovieDetail

--- src/app/movie_detail/[id]/_components/MovieInformation.tsx
+++ src/app/movie_detail/[id]/_components/MovieInformation.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Card, CardBody, Image, Text } from '@yamada-ui/react';
+import { Box, Card, CardBody, Flex, Image, Text } from '@yamada-ui/react';
 import { movieCard } from '@/mock/movie/mock';
 
 export const MovieInformation = () => {
@@ -7,23 +7,41 @@ export const MovieInformation = () => {
 
 	return (
 		<>
-			<Card borderRadius="0" m="20px 0" boxShadow="none" variant="outline">
+			<Box
+				display={'flex'}
+				alignItems={'center'}
+				p={'20px 15px'}
+				gap={'40px'}
+				bg={'caption.bg'}
+				color={'#fff'}
+			>
+				<Box fontSize={'1.2rem'} fontWeight={'bold'}>
+					{movieDetail.title}
+				</Box>
+				<Box display={'flex'} gap="20px">
+					<Text>上映時間:107分</Text>
+					<Text>監督:永岡智佳</Text>
+					<Text>
+						出演:【声の出演】高山みなみ／山崎和佳奈／小山力也／山口勝平 ほか
+					</Text>
+				</Box>
+			</Box>
+			<Box display={'flex'} gap={'10px'} bg={'caption.bg'} color={'#fff'}>
 				<Image
 					objectFit="cover"
 					src={movieDetail.movie_wide_image}
 					alt="movieImage"
-					w="100%"
+					w="50%"
 					aspectRatio="16/9"
+					p={'0 0 10px 10px'}
 				/>
-				<CardBody>
-					<Box h="200px">
-						<Text fontSize="1.4rem" fontWeight="bold">
-							{movieDetail.title}
-						</Text>
-						<Text>{movieDetail.detail}</Text>
-					</Box>
-				</CardBody>
-			</Card>
+				<Box h="200px" p={'0 10px 10px 0'}>
+					<Text fontSize="1.2rem" fontWeight="bold">
+						あらすじ
+					</Text>
+					<Text>{movieDetail.detail}</Text>
+				</Box>
+			</Box>
 		</>
 	);
 };
