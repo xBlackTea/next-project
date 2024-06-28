c317a58659b1002e4fb761d4be14c6a66fd1e80c
yamatatsu1215 <tatsuyama3140@gmail.com>
2024-06-28 12:25:09 +0900

    fix top/backend

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { Box, useBreakpoint } from '@yamada-ui/react';
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { movieCard } from '@/mock/movie/mock';
 import { fetchMovieResponse } from '@/mock/movie/MovieInterface';
 import {
@@ -14,9 +14,31 @@ import TheaterList from './_components/block/TheaterList';
 import ShopList from './_components/block/ShopList';
 import { EventList } from './_components/block/EventList';
 import { ServiceList } from './_components/block/ServiceList';
+import { fetchMovie } from './hooks/useMovie';
+
+interface Movie {
+	movie_id: number;
+	movie_image: string;
+	movie_name: string;
+}
 
 export default function Page() {
+	const [movies, setMovies] = useState<Movie[]>([]);
 	const breakpoint = useBreakpoint();
+
+	useEffect(() => {
+		const fetchData = async () => {
+			const moviesData = await fetchMovie();
+
+			const formattedMovie: Movie[] = moviesData.map((movie: any) => ({
+				id: movie.movie_id,
+				movie_image: movie.movie_image1,
+				movie_name: movie.movie_name,
+			}));
+			setMovies(formattedMovie);
+		};
+		fetchData();
+	}, []);
 
 	return (
 		<>
@@ -30,12 +52,12 @@ export default function Page() {
 					justifyContent="space-between"
 					alignItems="center"
 				>
-					{movieCard.map((data: fetchMovieResponse) => (
+					{movies.map((movie: Movie) => (
 						<MovieCard
-							key={data.id}
-							movie_image={data.movie_image}
-							title={data.title}
-							id={data.id}
+							key={movie.movie_id}
+							movie_image={movie.movie_image}
+							title={movie.movie_name}
+							id={movie.movie_id}
 						/>
 					))}
 				</Box>
