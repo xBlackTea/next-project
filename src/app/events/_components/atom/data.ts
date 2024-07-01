4573f03d9f453fa63baa561e151941962fac5b25
10yu30 <yuu2003151030@gmail.com>
2024-07-01 16:22:19 +0900

    add events update

---
+++ src/app/events/_components/atom/data.ts
@@ -0,0 +1,26 @@
+export const pickUpImages = [
+	'pilotfilm.jpg',
+	'pilotfilm.jpg',
+	'pilotfilm.jpg',
+	'pilotfilm.jpg',
+];
+
+export const pickUpDetails = ['11', '12', '13', '14'];
+
+export const limitedImages = [
+	'pilotfilm.jpg',
+	'pilotfilm.jpg',
+	'pilotfilm.jpg',
+	'pilotfilm.jpg',
+];
+
+export const limitedDetails = ['21', '22', '23', '24'];
+
+export const permanentImages = [
+	'pilotfilm.jpg',
+	'pilotfilm.jpg',
+	'pilotfilm.jpg',
+	'pilotfilm.jpg',
+];
+
+export const permanentDetails = ['31', '32', '33', '34'];
