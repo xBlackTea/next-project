b41a831a8ed39deda248d98f45a11925e7691db2
yamatatsu1215 <tatsuyama3140@gmail.com>
2024-07-04 22:17:19 +0900

    fix mypage backend effect

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,14 +1,31 @@
+'use client';
+
 import { Box, Center } from '@yamada-ui/react';
 import React from 'react';
 import UserInfo from './_components/block/UserInfo';
 import { TicketInfo } from './_components/block/TicketInfo';
+import useUserId from '../hooks/useUserId';
 
-const page = () => {
+const Page = () => {
+	const { user, schedules, loading, error } = useUserId();
+
+	if (loading) {
+		return <p>Loading...</p>; // ローディング中
+	}
+
+	if (error) {
+		return <p>Error: {error}</p>; // エラーが発生した場合
+	}
+
+	if (!user) {
+		return <p>No user or schedules found</p>; // ユーザまたはスケジュールが見つからない場合
+	}
+
 	return (
 		<Box m="0 auto">
 			<Center
 				style={{
-					width: ' 100%',
+					width: '100%',
 					height: '30px',
 					margin: '0 auto',
 					borderRadius: '2px',
@@ -29,11 +46,10 @@ const page = () => {
 				</p>
 			</Center>
 
-			<UserInfo />
-
-			<TicketInfo />
+			<UserInfo user={user} />
+			<TicketInfo schedules={schedules} />
 		</Box>
 	);
 };
 
-export default page;
+export default Page;
