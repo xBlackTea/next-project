a3603bcb67ce790d19abe22ff3390480bbcb703c
xBlackTea <xBlackTea.Official@gmail.com>
2024-07-19 12:08:52 +0900

    project05

--- src/app/movies/[id]/_components/MovieInformation.tsx
+++ src/app/movies/[id]/_components/MovieInformation.tsx
@@ -1,9 +1,9 @@
 // 'use client';
 // import React from 'react';
-// import { Box, Card, CardBody, Flex, Image, Text } from '@yamada-ui/react';
+// import { Box, Image, Text, useBreakpointValue } from '@yamada-ui/react';
 // import { movieCard } from '@/mock/movie/mock';
 
-// interface MovieInformationProps {
+// type MovieInformationProps = {
 // 	movie: {
 // 		movie_id: number;
 // 		movie_name: string;
@@ -12,58 +12,87 @@
 // 		movie_image1: string;
 // 		movie_image2: string;
 // 	};
-// }
+// };
 
-// export const MovieInformation: React.FC<MovieInformationProps> = ({
-// 	movie,
-// }) => {
+// export function MovieInformation({ movie }: MovieInformationProps) {
 // 	const movieDetail = movieCard[0];
+
 // 	console.log(decodeURIComponent(movie.movie_image2));
 
+// 	const breakpoint = useBreakpointValue({ base: 'base', sm: 'sm', md: 'md' });
+// 	const isSmallScreen = ['sm', 'md'].includes(breakpoint);
+
 // 	return (
-// 		<>
+// 		<Box
+// 			width="100%"
+// 			height="auto"
+// 			marginTop="15px"
+// 			padding="10px 0"
+// 			borderRadius="2px"
+// 			border="solid 1px #ddd"
+// 			backgroundColor="#fff"
+// 		>
 // 			<Box
-// 				display={'flex'}
-// 				alignItems={'center'}
-// 				p={'20px 15px'}
-// 				gap={'40px'}
-// 				bg={'caption.bg'}
-// 				color={'#fff'}
+// 				className="title-box"
+// 				width="calc(100% - 20px)"
+// 				height="40px"
+// 				marginLeft="10px"
+// 				borderRadius="2px"
+// 				backgroundColor="#111"
 // 			>
-// 				<Box fontSize={'1.2rem'} fontWeight={'bold'}>
+// 				<p
+// 					style={{
+// 						paddingLeft: '10px',
+// 						fontSize: '30px',
+// 						lineHeight: '1.2',
+// 						color: '#fff',
+// 					}}
+// 				>
 // 					{movie.movie_name}
-// 				</Box>
-// 				<Box display={'flex'} gap="20px">
-// 					<Text>上映時間:{movie.movie_time}</Text>
-// 					<Text>監督:永岡智佳</Text>
-// 					<Text>
-// 						出演:【声の出演】高山みなみ／山崎和佳奈／小山力也／山口勝平 ほか
-// 					</Text>
-// 				</Box>
+// 				</p>
 // 			</Box>
-// 			<Box display={'flex'} gap={'10px'} bg={'caption.bg'} color={'#fff'}>
-// 				<Image
-// 					objectFit="cover"
-// 					src={`/${movie.movie_image2}`}
-// 					alt="movieImage"
-// 					w="50%"
-// 					aspectRatio="16/9"
-// 					p={'0 0 10px 10px'}
-// 				/>
-// 				<Box h="200px" p={'0 10px 10px 0'}>
-// 					<Text fontSize="1.2rem" fontWeight="bold">
+// 			<Box
+// 				display="flex"
+// 				flexDirection={isSmallScreen ? 'column' : 'row'}
+// 				padding="10px 10px 0 10px"
+// 				gap="10px"
+// 			>
+// 				<Box flex={isSmallScreen ? '1' : '4'} width="100%">
+// 					<Image
+// 						objectFit="cover"
+// 						src={`/${movie.movie_image2}`}
+// 						alt="movieImage"
+// 						width="100%"
+// 						aspectRatio="16/9"
+// 						style={{ borderRadius: '2px', objectFit: 'cover' }}
+// 					/>
+// 				</Box>
+
+// 				<Box flex={isSmallScreen ? '1' : '6'}	borderRadius="2px" border="solid 1px #ddd">
+// 					<Text fontSize="1.2rem" fontWeight="bold" padding="10px 10px 0px 10px">
+// 						上映詳細
+// 					</Text>
+// 					<Text padding="0 10px 0 10px">
+// 						上映時間: {movie.movie_time} 分
+// 					</Text>
+// 					<Text padding="0 10px 0 10px">
+// 						監督: 永岡智佳 出演:【声の出演】高山みなみ／山崎和佳奈／小山力也／山口勝平 ほか
+// 					</Text>
+// 					<Text fontSize="1.2rem" fontWeight="bold" padding="10px 10px 0px 10px">
 // 						あらすじ
 // 					</Text>
-// 					<Text>{movie.movie_detail}</Text>
+// 					<Text padding="0 10px 10px 10px">
+// 						{movie.movie_detail}
+// 					</Text>
 // 				</Box>
 // 			</Box>
-// 		</>
+// 		</Box>
 // 	);
-// };
+// }
 
 'use client';
 import React from 'react';
-import { Box, Image, Text, useBreakpointValue } from '@yamada-ui/react';
+import { Box, Image, Text, useBreakpoint } from '@yamada-ui/react';
 import { movieCard } from '@/mock/movie/mock';
 
 type MovieInformationProps = {
@@ -82,21 +111,42 @@ export function MovieInformation({ movie }: MovieInformationProps) {
 
 	console.log(decodeURIComponent(movie.movie_image2));
 
-	const breakpoint = useBreakpointValue({ base: 'base', sm: 'sm', md: 'md' });
+	const breakpoint = useBreakpoint();
 	const isSmallScreen = ['sm', 'md'].includes(breakpoint);
 
 	return (
 		<Box
 			width="100%"
+			height="auto"
 			marginTop="15px"
-			bg="#111"
-			color="#fff"
+			padding="10px 0"
 			borderRadius="2px"
-			padding="10px"
+			border="solid 1px #ddd"
+			backgroundColor="#fff"
 		>
+			<Box
+				className="title-box"
+				width="calc(100% - 20px)"
+				height="40px"
+				marginLeft="10px"
+				borderRadius="2px"
+				backgroundColor="#111"
+			>
+				<p
+					style={{
+						paddingLeft: '10px',
+						fontSize: '30px',
+						lineHeight: '1.2',
+						color: '#fff',
+					}}
+				>
+					{movie.movie_name}
+				</p>
+			</Box>
 			<Box
 				display="flex"
 				flexDirection={isSmallScreen ? 'column' : 'row'}
+				padding="10px 10px 0 10px"
 				gap="10px"
 			>
 				<Box flex={isSmallScreen ? '1' : '4'} width="100%">
@@ -106,24 +156,35 @@ export function MovieInformation({ movie }: MovieInformationProps) {
 						alt="movieImage"
 						width="100%"
 						aspectRatio="16/9"
+						style={{ borderRadius: '2px', objectFit: 'cover' }}
 					/>
 				</Box>
 
-				<Box flex={isSmallScreen ? '1' : '6'} padding="0 10px 10px 0">
-					<Box width="100%" height="auto" paddingBottom="10px">
-						<Text fontSize="1.2rem" fontWeight="bold">
-							{movie.movie_name}
-						</Text>
-						<Text>上映時間: {movie.movie_time} 分</Text>
-						<Text>
-							監督: 永岡智佳
-							出演:【声の出演】高山みなみ／山崎和佳奈／小山力也／山口勝平 ほか
-						</Text>
-					</Box>
-					<Text fontSize="1.2rem" fontWeight="bold">
+				<Box
+					flex={isSmallScreen ? '1' : '6'}
+					borderRadius="2px"
+					border="solid 1px #ddd"
+				>
+					<Text
+						fontSize="1.2rem"
+						fontWeight="bold"
+						padding="10px 10px 0px 10px"
+					>
+						上映詳細
+					</Text>
+					<Text padding="0 10px 0 10px">上映時間: {movie.movie_time} 分</Text>
+					<Text padding="0 10px 0 10px">
+						監督: 永岡智佳
+						出演:【声の出演】高山みなみ／山崎和佳奈／小山力也／山口勝平 ほか
+					</Text>
+					<Text
+						fontSize="1.2rem"
+						fontWeight="bold"
+						padding="10px 10px 0px 10px"
+					>
 						あらすじ
 					</Text>
-					<Text>{movie.movie_detail}</Text>
+					<Text padding="0 10px 10px 10px">{movie.movie_detail}</Text>
 				</Box>
 			</Box>
 		</Box>
