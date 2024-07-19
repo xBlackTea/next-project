22baaae492ec5dabc7b6c62f03e6212ce5207cb0
xBlackTea <xBlackTea.Official@gmail.com>
2024-07-19 11:03:20 +0900

    project01

--- src/app/movies/_components/MovieCard.tsx
+++ src/app/movies/_components/MovieCard.tsx
@@ -1,209 +1,87 @@
 // import React from 'react';
 // import {
-// 	Card,
-// 	CardFooter,
-// 	CardHeader,
-// 	Image,
-// 	Text,
-// 	useBreakpoint,
-// 	useColorMode,
-// 	Box,
+//   Card,
+//   CardFooter,
+//   CardHeader,
+//   Text,
+//   useColorMode,
+//   Box,
+//   Image,
 // } from '@yamada-ui/react';
 
-// type issueMovieProps = {
-// 	key: number;
-// 	movie_image: string;
-// 	movie_name: string;
-// 	movie_id: number;
+// type MovieCardProps = {
+//   key: number;
+//   movie_image: string;
+//   movie_name: string;
+//   movie_id: number;
 // };
 
-// export function MovieCard(props: issueMovieProps) {
-// 	const breakpoint = useBreakpoint();
-// 	const { colorMode } = useColorMode();
+// export function MovieCard(props: MovieCardProps) {
+//   const { colorMode } = useColorMode();
 
-// 	let cardWidth;
-// 	let fontSize;
-
-// 	switch (breakpoint) {
-// 		case 'md':
-// 			cardWidth = '100%';
-// 			fontSize = 'md';
-// 			break;
-// 		case 'sm':
-// 			cardWidth = '100%';
-// 			fontSize = 'sm';
-// 			break;
-// 		default:
-// 			cardWidth = '490px';
-// 			fontSize = 'sm';
-// 	}
-
-// 	return (
-// 		<Card
-// 			w={cardWidth}
-// 			borderRadius="2px"
-// 			sx={{
-// 				'&:hover .img': {
-// 					transition: colorMode === 'dark' ? 'filter 0.3s ease-in-out' : '',
-// 					filter: colorMode === 'dark' ? 'brightness(100%)' : '',
-// 				},
-// 				'&:not(:hover) .img': {
-// 					transition: colorMode === 'dark' ? 'filter 0.3s ease-in-out' : '',
-// 					filter: colorMode === 'dark' ? 'brightness(70%)' : '',
-// 				},
-// 			}}
-// 			overflow="hidden"
-// 			cursor="pointer"
-// 			variant="outline"
-// 			color="movieCard.color"
-// 			backgroundColor="movieCard.bg"
-// 		>
-// 			<CardHeader
-// 				display="flex"
-// 				justifyContent="center"
-// 				margin="10px"
-// 				padding="0px"
-// 			>
-// 				<Image
-// 					className="img"
-// 					src={props.movie_image}
-// 					width="100%"
-// 					height="auto"
-// 					style={{ objectFit: 'cover', borderRadius: '2px' }}
-// 					alt={props.movie_name}
-// 				/>
-// 			</CardHeader>
-// 			<CardFooter
-// 				display="flex"
-// 				justifyContent="center"
-// 				alignItems="center"
-// 				backgroundColor="#111"
-// 				color="#fff"
-// 				height="40px"
-// 				margin="0 10px 10px 10px"
-// 				paddingBottom="5px"
-// 				borderRadius="2px"
-// 			>
-// 				<Box
-// 					display="flex"
-// 					justifyContent="center"
-// 					alignItems="center"
-// 					height="100%"
-// 					width="100%"
-// 				>
-// 					<Text fontSize={fontSize} isTruncated textAlign="center" width="100%">
-// 						{props.movie_name}
-// 					</Text>
-// 				</Box>
-// 			</CardFooter>
-// 		</Card>
-// 	);
+//   return (
+//     <Card
+//       width="100%"
+//       borderRadius="2px"
+//       sx={{
+//         '&:hover .img': {
+//           transition: colorMode === 'dark' ? 'filter 0.3s ease-in-out' : '',
+//           filter: colorMode === 'dark' ? 'brightness(100%)' : '',
+//         },
+//         '&:not(:hover) .img': {
+//           transition: colorMode === 'dark' ? 'filter 0.3s ease-in-out' : '',
+//           filter: colorMode === 'dark' ? 'brightness(70%)' : '',
+//         },
+//       }}
+//       overflow="hidden"
+//       cursor="pointer"
+//       variant="outline"
+//       color="movieCard.color"
+//       backgroundColor="#fff"
+//     >
+//       <CardHeader
+//         display="flex"
+//         justifyContent="center"
+//         margin="10px"
+//         padding="0px"
+//       >
+//         <Box width="100%" height="auto" overflow="hidden" borderRadius="2px">
+//           <Image
+//             className="img"
+//             src={props.movie_image}
+//             alt={props.movie_name}
+//             width="100%"
+//             height="auto"
+//             style={{ objectFit: 'cover' }}
+//           />
+//         </Box>
+//       </CardHeader>
+//       <CardFooter
+//         display="flex"
+//         justifyContent="center"
+//         alignItems="center"
+//         backgroundColor="#111"
+//         color="#fff"
+//         height="40px"
+//         margin="0 10px 10px 10px"
+//         paddingBottom="5px"
+//         borderRadius="2px"
+//       >
+//         <Box
+//           display="flex"
+//           justifyContent="center"
+//           alignItems="center"
+//           height="100%"
+//           width="100%"
+//         >
+//           <Text fontSize="sm" isTruncated textAlign="center" width="100%">
+//             {props.movie_name}
+//           </Text>
+//         </Box>
+//       </CardFooter>
+//     </Card>
+//   );
 // }
-
-// ========================================================
-
-// import React from 'react';
-// import {
-// 	Card,
-// 	CardFooter,
-// 	CardHeader,
-// 	Text,
-// 	useBreakpoint,
-// 	useColorMode,
-// 	Box,
-// 	AspectRatio,
-// } from '@yamada-ui/react';
-
-// type issueMovieProps = {
-// 	key: number;
-// 	movie_image?: string;
-// 	movie_name?: string;
-// 	movie_id?: number;
-// };
-
-// export function MovieCard(props: issueMovieProps) {
-// 	const breakpoint = useBreakpoint();
-// 	const { colorMode } = useColorMode();
-
-// 	let cardWidth;
-// 	let fontSize;
-
-// 	switch (breakpoint) {
-// 		case 'md':
-// 			cardWidth = '100%';
-// 			fontSize = 'md';
-// 			break;
-// 		case 'sm':
-// 			cardWidth = '100%';
-// 			fontSize = 'sm';
-// 			break;
-// 		default:
-// 			cardWidth = 'calc(33.3333% - 10px)';
-// 			fontSize = 'sm';
-// 	}
-
-// 	return (
-// 		<Card
-// 			w={cardWidth}
-// 			borderRadius="2px"
-// 			sx={{
-// 				'&:hover .img': {
-// 					transition: colorMode === 'dark' ? 'filter 0.3s ease-in-out' : '',
-// 					filter: colorMode === 'dark' ? 'brightness(100%)' : '',
-// 				},
-// 				'&:not(:hover) .img': {
-// 					transition: colorMode === 'dark' ? 'filter 0.3s ease-in-out' : '',
-// 					filter: colorMode === 'dark' ? 'brightness(70%)' : '',
-// 				},
-// 			}}
-// 			overflow="hidden"
-// 			cursor="pointer"
-// 			variant="outline"
-// 			color="movieCard.color"
-// 			backgroundColor="#fff"
-// 		>
-// 			<CardHeader
-// 				display="flex"
-// 				justifyContent="center"
-// 				margin="10px"
-// 				padding="0px"
-// 			>
-// 				<AspectRatio ratio={5 / 7} width="100%">
-// 					<Box
-// 						className="img"
-// 						backgroundColor="#ddd"
-// 						borderRadius="2px"
-// 					/>
-// 				</AspectRatio>
-// 			</CardHeader>
-// 			<CardFooter
-// 				display="flex"
-// 				justifyContent="center"
-// 				alignItems="center"
-// 				backgroundColor="#111"
-// 				color="#fff"
-// 				height="40px"
-// 				margin="0 10px 10px 10px"
-// 				paddingBottom="5px"
-// 				borderRadius="2px"
-// 			>
-// 				<Box
-// 					display="flex"
-// 					justifyContent="center"
-// 					alignItems="center"
-// 					height="100%"
-// 					width="100%"
-// 				>
-// 					<Text fontSize={fontSize} isTruncated textAlign="center" width="100%">
-// 						{props.movie_name || 'Placeholder'}
-// 					</Text>
-// 				</Box>
-// 			</CardFooter>
-// 		</Card>
-// 	);
-// }
-
-// ========================================================
 
 import React from 'react';
 import {
@@ -211,44 +89,24 @@ import {
 	CardFooter,
 	CardHeader,
 	Text,
-	useBreakpoint,
 	useColorMode,
 	Box,
-	AspectRatio,
 	Image,
 } from '@yamada-ui/react';
 
-type issueMovieProps = {
+type MovieCardProps = {
 	key: number;
-	movie_image?: string;
-	movie_name?: string;
-	movie_id?: number;
+	movie_image: string;
+	movie_name: string;
+	movie_id: number;
 };
 
-export function MovieCard(props: issueMovieProps) {
-	const breakpoint = useBreakpoint();
+export function MovieCard(props: MovieCardProps) {
 	const { colorMode } = useColorMode();
-
-	let cardWidth;
-	let fontSize;
-
-	switch (breakpoint) {
-		case 'md':
-			cardWidth = '100%';
-			fontSize = 'md';
-			break;
-		case 'sm':
-			cardWidth = '100%';
-			fontSize = 'sm';
-			break;
-		default:
-			cardWidth = 'calc(33.3333% - 10px)';
-			fontSize = 'sm';
-	}
 
 	return (
 		<Card
-			w={cardWidth}
+			width="100%"
 			borderRadius="2px"
 			sx={{
 				'&:hover .img': {
@@ -272,16 +130,16 @@ export function MovieCard(props: issueMovieProps) {
 				margin="10px"
 				padding="0px"
 			>
-				<AspectRatio ratio={5 / 7} width="100%">
+				<Box width="100%" height="auto" overflow="hidden" borderRadius="2px">
 					<Image
 						className="img"
 						src={props.movie_image}
 						alt={props.movie_name}
 						width="100%"
 						height="auto"
-						borderRadius="2px"
+						style={{ objectFit: 'cover' }}
 					/>
-				</AspectRatio>
+				</Box>
 			</CardHeader>
 			<CardFooter
 				display="flex"
@@ -301,8 +159,8 @@ export function MovieCard(props: issueMovieProps) {
 					height="100%"
 					width="100%"
 				>
-					<Text fontSize={fontSize} isTruncated textAlign="center" width="100%">
-						{props.movie_name || 'Placeholder'}
+					<Text fontSize="sm" isTruncated textAlign="center" width="100%">
+						{props.movie_name}
 					</Text>
 				</Box>
 			</CardFooter>
