8420d2aae02e2a699a05c7e5a932b806008adb96
xBlackTea <xBlackTea.Official@gmail.com>
2024-09-06 10:38:16 +0900

    fix: 347 トップページ作品案内レイアウト修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,19 @@
 'use client';
-import { Box, useBreakpoint } from '@yamada-ui/react';
-
+import { Box, useBreakpoint, AspectRatio } from '@yamada-ui/react';
 import React, { useEffect, useState } from 'react';
-import { movieCard } from '@/mock/movie/mock';
-import { fetchMovieResponse } from '@/mock/movie/MovieInterface';
 import {
-	MovieCard,
 	TopSectionContainer,
 	TopSectionHeader,
 	TopSlider,
 } from './_components/block';
+import { MovieCard } from './movies/_components/MovieCard';
 import TheaterList from './_components/block/TheaterList';
 import ShopList from './_components/block/ShopList';
 import { EventList } from './_components/block/EventList';
 import { ServiceList } from './_components/block/ServiceList';
 import { fetchMovie } from './hooks/useMovie';
 import Link from 'next/link';
+import Placeholder from './movies/_components/Placeholder';
 
 interface Movie {
 	movie_id: number;
@@ -25,21 +23,33 @@ interface Movie {
 
 export default function Page() {
 	const [movies, setMovies] = useState<Movie[]>([]);
+	const [loading, setLoading] = useState(true);
 	const breakpoint = useBreakpoint();
 
 	useEffect(() => {
 		const fetchData = async () => {
 			const moviesData = await fetchMovie();
-
-			const formattedMovie: Movie[] = moviesData.map((movie: any) => ({
+			const formattedMovies: Movie[] = moviesData.map((movie: any) => ({
 				movie_id: movie.movie_id,
 				movie_image: movie.movie_image1,
 				movie_name: movie.movie_name,
 			}));
-			setMovies(formattedMovie);
+			setMovies(formattedMovies);
+			setLoading(false);
 		};
 		fetchData();
 	}, []);
+
+	const getContainerWidth = (breakpoint: string) => {
+		switch (breakpoint) {
+			case 'sm':
+				return '100%';
+			case 'md':
+				return 'calc(50% - 7.5px)';
+			default:
+				return 'calc(33.3333% - 10px)';
+		}
+	};
 
 	return (
 		<>
@@ -52,17 +62,29 @@ export default function Page() {
 					display="flex"
 					justifyContent="space-between"
 					alignItems="center"
+					marginTop="15px"
+					gap="15px"
 				>
-					{movies.map((movie: Movie) => (
-						<Link key={movie.movie_id} href={`./movies/${movie.movie_id}`}>
-							<MovieCard
-								key={movie.movie_id}
-								movie_image={movie.movie_image}
-								title={movie.movie_name}
-								id={movie.movie_id}
-							/>
-						</Link>
-					))}
+					{loading
+						? Array.from({ length: 6 }).map((_, index) => (
+								<Box key={index} width={getContainerWidth(breakpoint)}>
+									<AspectRatio ratio={5 / 7}>
+										<Placeholder width="100%" height="100%" />
+									</AspectRatio>
+								</Box>
+							))
+						: movies.map((movie: Movie) => (
+								<Box key={movie.movie_id} width={getContainerWidth(breakpoint)}>
+									<Link href={`./movies/${movie.movie_id}`}>
+										<MovieCard
+											key={movie.movie_id}
+											movie_id={movie.movie_id}
+											movie_image={movie.movie_image}
+											movie_name={movie.movie_name}
+										/>
+									</Link>
+								</Box>
+							))}
 				</Box>
 			</TopSectionContainer>
 
