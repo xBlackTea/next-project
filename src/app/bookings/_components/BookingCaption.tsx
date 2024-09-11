8ab77aac6694b7f6b96305dea981e5b92293b530
xBlackTea <xBlackTea.Official@gmail.com>
2024-09-12 01:14:27 +0900

    fix: レイアウト

--- src/app/bookings/_components/BookingCaption.tsx
+++ src/app/bookings/_components/BookingCaption.tsx
@@ -1,10 +1,18 @@
-import { Box } from '@yamada-ui/react';
+import { Box, Text } from '@yamada-ui/react';
 import React from 'react';
 
 export const BookingCaption = () => {
 	return (
-		<Box w="100%" p="15px" m="20px 0" color="#fff" bgColor="caption.bg">
-			座席予約
+		<Box
+			height="60px"
+			borderRadius="2px"
+			backgroundColor="#111"
+			width="100%"
+			marginTop="15px"
+		>
+			<Text paddingLeft="10px" fontSize="30px" lineHeight="1.8" color="#fff">
+				座席予約
+			</Text>
 		</Box>
 	);
 };
